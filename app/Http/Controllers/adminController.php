d18b35ea35f96294e1019411078f5672bf49e96e
rishi_dev <rishishekhawat.img@gmail.com>
2024-10-08 16:13:15 +0530

    mail sent and error on contact us

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\DB;
 use App\Models\Blog;
 use App\Models\ContactUs;
 use Illuminate\Http\Request;
+use App\Mail\ContactUsMail;
+use App\Mail\PHPMailerService;
+use Illuminate\Support\Facades\Mail as FacadesMail;
+// use Mail;
 use Symfony\Component\CssSelector\Node\FunctionNode;
 
 class adminController extends Controller
@@ -24,17 +28,24 @@ class adminController extends Controller
     }
     public function submitblogform(Request $request)
     {
-        
         $validate = $request->validate([
             'name' => 'required|string|max:100',
             'email' => 'required|email|max:100',
-            'phone' => 'required|string|max:15',
+            'phone' => 'required|max:15|regex:/^[7-9][0-9]{9}$/',
             'cmpname' => 'required|string|max:100',
             'message' => 'required|string',
         ]);
-            return redirect()->back();
-            ContactUs::create($validate); 
-            // return back()->with('success', 'Thank you for contacting us!');
+        ContactUs::create($validate);
+        $mailService = new ContactUsMail();
+        $to = 'rishishekhawat.img@gmail.com';
+        $subject = 'You have receive Notification for contact';
+        $body = $request->phone.'<- This is contact number, & email is ->'.$request->email;
+
+        $response = $mailService->sendMail($to, $subject, $body);
+        return response()->json([
+            'success' => true,
+            'message' => 'Your request hasa been send'
+        ], 200);
         }
     
     public function contactusform(Request $request)
